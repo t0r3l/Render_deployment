ccfc10ba61a17038b562c4dc90bb41bae0256d8c
t0r3l <109417991+t0r3l@users.noreply.github.com>
2024-11-29 17:57:01 +0100

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // Database
 const sequelize = new Sequelize(
-  '', // TODO: database connection string
+  'postgresql://t0r3l:x92xJ0HEnNex5HxzAxak5weirpaTggOL@dpg-ct4uoj1opnds73d4h6ig-a/db1_i7b5', // TODO: database connection string
   {
     dialect: 'postgres',
     dialectOptions: {
